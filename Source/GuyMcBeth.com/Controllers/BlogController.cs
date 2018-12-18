507d5f8f0040c930f398487e721f63737a0144f2
jcmcbeth <joel.mcbeth@gmail.com>
2018-12-18 18:16:33 -0500

    Initial commit.

---
+++ Source/GuyMcBeth.com/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿namespace GuyMcBeth.com.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Source/GuyMcBeth.com/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿namespace GuyMcBeth.com.Controllers
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using GuyMcBeth.com.Models;
+    using System.Reflection;
+    using System.IO;
+    using System;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult LastUpdated()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            FileInfo fileInfo = new FileInfo(assembly.Location);
+            DateTime lastModified = fileInfo.LastWriteTime;
+
+            return this.Json(lastModified);
+        }
+
+        public IActionResult Food()
+        {
+            return this.Redirect("https://1drv.ms/w/s!Akh_WeVgv7PQhYJx1an-aLM3H5ZZ7w");
+        }
+    }
+}
