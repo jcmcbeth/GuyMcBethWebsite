cd898025fa1e1391f19326d5dc4f8aea6c75b2b0
Joel McBeth <joel.mcbeth@gmail.com>
2019-03-27 17:22:30 -0400

    Updating all pages to be current.

---
+++ Source/GuyMcBeth.com/Controllers/CareController.cs
@@ -0,0 +1,12 @@
+﻿namespace GuyMcBeth.com.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CareController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
