e8e7b448c4b88c7238b86de79fd8fccc9794637a
jcmcbeth <joel.mcbeth@gmail.com>
2019-04-23 19:33:28 -0400

    Last updated date should be based on the most recent of any file in the application's deployment directory.

--- Source/GuyMcBeth.com/Controllers/HomeController.cs
+++ Source/GuyMcBeth.com/Controllers/HomeController.cs
@@ -6,9 +6,20 @@
     using System.Reflection;
     using System.IO;
     using System;
+    using Microsoft.AspNetCore.Hosting;
+    using System.Linq;
+    using System.Collections.Generic;
+    using Microsoft.Extensions.FileProviders;
 
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
         public IActionResult Index()
         {
             return this.View();
@@ -21,11 +32,38 @@
 
         public IActionResult LastUpdated()
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            FileInfo fileInfo = new FileInfo(assembly.Location);
-            DateTime lastModified = fileInfo.LastWriteTime;
+            var lastModified = this.GetContentFiles().Max(fi => fi.LastModified);
 
             return this.Json(lastModified);
+        }
+
+        private IEnumerable<IFileInfo> GetContentFiles()
+        {
+            var directories = new Queue<string>();
+            var files = new List<IFileInfo>();
+
+            directories.Enqueue("");
+
+            while (directories.Count > 0)
+            {
+                var directory = directories.Dequeue();
+
+                var items = this.hostingEnvironment
+                    .ContentRootFileProvider
+                    .GetDirectoryContents(directory)
+                    .ToList();
+
+                var directoryFiles = items.Where(fi => !fi.IsDirectory);
+
+                files.AddRange(directoryFiles);
+
+                foreach (var newDirectory in items.Where(fi => fi.IsDirectory))
+                {
+                    directories.Enqueue(directory + "\\" + newDirectory.Name);
+                }
+            }
+
+            return files;
         }
     }
 }
